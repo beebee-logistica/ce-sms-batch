03d1d24eb5e12d1be6ecbeb9a75c1b0dfb743276
vizeke <vizeke@gmail.com>
2019-03-12 19:09:32 -0300

    fix(content-script): localstorage token location

--- content-script.js
+++ content-script.js
@@ -1,26 +1,29 @@
-console.log( 'bee bee chrome extension' );
-
+console.log('===== Start of bee bee sms extension content-script');
+console.log(location.host);
 let enviroment = undefined;
 
-if ( location.host.startsWith( 'dev' ) ) {
+if (location.host.startsWith('dev')) {
     enviroment = 'dev';
-} else if ( location.host.startsWith( 'beta' ) ) {
+} else if (location.host.startsWith('beta')) {
     enviroment = 'beta';
-} else if ( location.host.startsWith( 'beebee' ) ) {
+} else if (location.host.startsWith('beebee')) {
     enviroment = 'prod';
 }
 
-if ( enviroment ) {
-    chrome.runtime.sendMessage( { type: 'enviroment', enviroment: enviroment }, ( response ) => {
-        console.log( response );
-    } );
+console.log(enviroment);
+if (enviroment) {
+    chrome.runtime.sendMessage({ type: 'enviroment', enviroment: enviroment }, (response) => {
+        console.log(response);
+    });
 }
 
-let tokenStorage = "bee-jwt-token-" + enviroment;
-let token = localStorage[ tokenStorage ];
+let akitaStateName = 'akita-state-' + enviroment;
+let akitaState = JSON.parse(localStorage[akitaStateName]);
 
-if ( token ) {
-    chrome.runtime.sendMessage( { type: 'token', token: token }, ( response ) => {
-        console.log( response );
-    } );
+console.log(akitaState.auth.token);
+if (akitaState && akitaState.auth && akitaState.auth.token) {
+    chrome.runtime.sendMessage({ type: 'token', token: akitaState.auth.token }, (response) => {
+        console.log(response);
+    });
 }
+console.log('===== End of beebee sms extension content-stript')
